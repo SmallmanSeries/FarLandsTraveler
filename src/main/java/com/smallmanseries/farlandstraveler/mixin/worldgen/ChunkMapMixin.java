265f67b2457ef3f52eeac2870fad5bf43f78e6d0
OslorasKi <963499487@qq.com>
2026-02-07 20:53:34 +0800

    data driven far lands : world generator dynamic switching

--- src/main/java/com/smallmanseries/farlandstraveler/mixin/worldgen/ChunkMapMixin.java
+++ src/main/java/com/smallmanseries/farlandstraveler/mixin/worldgen/ChunkMapMixin.java
@@ -3,8 +3,10 @@ package com.smallmanseries.farlandstraveler.mixin.worldgen;
 import com.smallmanseries.farlandstraveler.common.DataRegister;
 import com.smallmanseries.farlandstraveler.common.worldgen.farlands.FarLands;
 import net.minecraft.server.level.ChunkMap;
+import net.minecraft.server.level.ServerLevel;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.chunk.ChunkAccess;
+import net.minecraft.world.level.chunk.ChunkGenerator;
 import net.minecraft.world.level.chunk.status.WorldGenContext;
 import net.minecraft.world.level.levelgen.NoiseBasedChunkGenerator;
 import org.slf4j.Logger;
@@ -26,15 +28,26 @@ public abstract class ChunkMapMixin {
     private void modifyGenerator(Args args){
         WorldGenContext context = args.get(0);
         ChunkAccess chunk = args.get(2);
-        if(context.generator() instanceof NoiseBasedChunkGenerator generator) {
-            if (context.level().dimension() == Level.OVERWORLD && chunk.getPos().getMinBlockX() > 12550824) {
-                generator = new NoiseBasedChunkGenerator(
-                        context.level().registryAccess().lookupOrThrow(DataRegister.FAR_LANDS).getValueOrThrow(FarLands.FAR_LANDS).biomeSource(),
-                        context.level().registryAccess().lookupOrThrow(DataRegister.FAR_LANDS).getValueOrThrow(FarLands.FAR_LANDS).settings()
-                );
-            }
-            WorldGenContext modifiedContext = new WorldGenContext(context.level(), generator, context.structureManager(), context.lightEngine(), context.mainThreadExecutor(), context.unsavedListener());
-            args.set(0, modifiedContext);
+        ChunkGenerator generator = context.generator();
+        ServerLevel level = context.level();
+        //边境之地
+        if (context.level().dimension() == Level.OVERWORLD &&
+                (chunk.getPos().getMinBlockX() > 12550824
+                || chunk.getPos().getMinBlockZ() > 12550824
+                || chunk.getPos().getMaxBlockX() < -12550824
+                || chunk.getPos().getMaxBlockZ() < -12550824)) {
+            generator = new NoiseBasedChunkGenerator(
+                    context.level().registryAccess().lookupOrThrow(DataRegister.FAR_LANDS).getValueOrThrow(FarLands.FAR_LANDS).biomeSource(),
+                    context.level().registryAccess().lookupOrThrow(DataRegister.FAR_LANDS).getValueOrThrow(FarLands.FAR_LANDS).settings()
+            );
+            level.getChunkSource().randomState();
         }
+        //遥远之地
+        //边缘之地
+        //边缘之角
+        //基岩海
+        WorldGenContext modifiedContext = new WorldGenContext(level, generator, context.structureManager(), context.lightEngine(), context.mainThreadExecutor(), context.unsavedListener());
+        args.set(0, modifiedContext);
+
     }
 }
---
+++ src/main/java/com/smallmanseries/farlandstraveler/mixin/worldgen/NoiseChunkMixin.java
@@ -0,0 +1,38 @@
+package com.smallmanseries.farlandstraveler.mixin.worldgen;
+
+import net.minecraft.world.level.levelgen.*;
+import net.minecraft.world.level.levelgen.blending.Blender;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(NoiseChunk.class)
+public abstract class NoiseChunkMixin {
+
+    @Unique
+    private NoiseRouter router;
+
+    // 如果在对应维度（还没做）且坐标大于指定值时，获取noise settings里的路由器，否则获取random里的（原版一律为random里的）
+    @Inject(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/RandomState;router()Lnet/minecraft/world/level/levelgen/NoiseRouter;"))
+    private void modify(int cellCountXZ, RandomState random, int firstNoiseX, int firstNoiseZ, NoiseSettings noiseSettings, DensityFunctions.BeardifierOrMarker beardifier, NoiseGeneratorSettings noiseGeneratorSettings, Aquifer.FluidPicker fluidPicker, Blender blendifier, CallbackInfo ci){
+        if((firstNoiseX > 12550824
+                || firstNoiseZ > 12550824
+                || firstNoiseX < -12550824
+                || firstNoiseZ < -12550824)) {// 判断条件还没加维度
+            this.router = noiseGeneratorSettings.noiseRouter();
+        } else {
+            this.router = random.router();
+        }
+    }
+
+    // 应用路由器
+    @Redirect(method = "<init>", at = @At(value = "INVOKE", target = "Lnet/minecraft/world/level/levelgen/RandomState;router()Lnet/minecraft/world/level/levelgen/NoiseRouter;"))
+    private NoiseRouter redirectRandomRouter(RandomState state) {
+        return this.router;
+    }
+
+
+}
