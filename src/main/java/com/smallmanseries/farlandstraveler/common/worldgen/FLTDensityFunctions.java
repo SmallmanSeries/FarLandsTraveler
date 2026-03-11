244dcde8fde918e197ff8341b4a2f9607a54e299
SmallmanSeries <963499487@qq.com>
2026-03-11 21:41:25 +0800

    feat: added box select density function

--- src/main/java/com/smallmanseries/farlandstraveler/common/worldgen/FLTDensityFunctions.java
+++ src/main/java/com/smallmanseries/farlandstraveler/common/worldgen/FLTDensityFunctions.java
@@ -3,6 +3,7 @@ package com.smallmanseries.farlandstraveler.common.worldgen;
 import com.mojang.serialization.MapCodec;
 import com.smallmanseries.farlandstraveler.FarLandsTraveler;
 import com.smallmanseries.farlandstraveler.common.worldgen.densityfunctions.BlendedNoiseOverflowable;
+import com.smallmanseries.farlandstraveler.common.worldgen.densityfunctions.BoxSelect;
 import com.smallmanseries.farlandstraveler.common.worldgen.densityfunctions.VirtualDensityFunction;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.world.level.levelgen.DensityFunction;
@@ -15,5 +16,7 @@ public class FLTDensityFunctions {
     static {
         FUNCTIONS.register("virtual_density_function", () -> VirtualDensityFunction.DATA_CODEC);
         FUNCTIONS.register("old_blended_noise_overflowable", () -> BlendedNoiseOverflowable.DATA_CODEC);
+        FUNCTIONS.register("box_select", () -> BoxSelect.DATA_CODEC);
+
     }
 }
