9a75d8d53ecca374ba3060bc803f2372f14d3055
Osloras Ki <75368186@qq.com>
2026-05-26 12:56:44 +0800

    fix: some types of blocks still have collision in fake chunks

--- src/main/java/com/smallmanseries/farlandstraveler/mixin/fakechunk/BlockStateBaseMixin.java
+++ src/main/java/com/smallmanseries/farlandstraveler/mixin/fakechunk/BlockStateBaseMixin.java
@@ -4,7 +4,6 @@ import com.smallmanseries.farlandstraveler.common.distance_phenomenon.FakeChunk;
 import net.minecraft.core.BlockPos;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.block.state.BlockBehaviour;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.shapes.CollisionContext;
 import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
@@ -13,11 +12,12 @@ import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
-@Mixin(BlockBehaviour.class)
-public class BlockBehaviourMixin {
-    @Inject(method = "getCollisionShape", at = @At("RETURN"), cancellable = true)
-    private void modifyCollision(BlockState state, BlockGetter level, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir){
-        if (FakeChunk.shouldDisableCollision(state, level, pos, context)){
+@Mixin(BlockBehaviour.BlockStateBase.class)
+public abstract class BlockStateBaseMixin {
+
+    @Inject(method = "getCollisionShape(Lnet/minecraft/world/level/BlockGetter;Lnet/minecraft/core/BlockPos;Lnet/minecraft/world/phys/shapes/CollisionContext;)Lnet/minecraft/world/phys/shapes/VoxelShape;", at = @At("RETURN"), cancellable = true)
+    private void modifyCollision(BlockGetter level, BlockPos pos, CollisionContext context, CallbackInfoReturnable<VoxelShape> cir){
+        if (FakeChunk.shouldDisableCollision(level.getBlockState(pos), level, pos, context)){
             cir.setReturnValue(Shapes.empty());
         }
         cir.setReturnValue(cir.getReturnValue());
