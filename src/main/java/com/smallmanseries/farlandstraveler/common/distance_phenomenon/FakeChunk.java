e5f6aec2c5a5de2916a752fbd969a4e9a6ff7610
Osloras Ki <75368186@qq.com>
2026-05-27 22:31:29 +0800

    feat: mob effect detect for fake chunks

--- src/main/java/com/smallmanseries/farlandstraveler/common/distance_phenomenon/FakeChunk.java
+++ src/main/java/com/smallmanseries/farlandstraveler/common/distance_phenomenon/FakeChunk.java
@@ -3,10 +3,10 @@ package com.smallmanseries.farlandstraveler.common.distance_phenomenon;
 import com.smallmanseries.farlandstraveler.common.misc.FLTAttachments;
 import com.smallmanseries.farlandstraveler.common.misc.FLTTags;
 import net.minecraft.core.BlockPos;
+import net.minecraft.world.effect.MobEffects;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EquipmentSlot;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.Mob;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.state.BlockState;
@@ -39,16 +39,24 @@ public class FakeChunk {
      * @return 布尔值，是否取消碰撞
      */
     public static boolean shouldDisableCollision(BlockState state, BlockGetter getter, BlockPos pos, CollisionContext context) {
+        // 对实体的判断
         if (context instanceof EntityCollisionContext entityContext) {
             Entity entity = entityContext.getEntity();
             if (entity != null && isInFakeChunk(entity.level(), pos) && !state.is(FLTTags.Blocks.DESOLID_EFFECT_NO_EFFECT)) {
-                if (entity instanceof LivingEntity living && !entity.getType().is(FLTTags.EntityTypes.DESOLID_EFFECT_IMMUNE)) { // 忽略免疫“去固体效应”的实体
-                    return !(  living.getItemBySlot(EquipmentSlot.BODY).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE) // 忽略穿着减免“去固体效应”的装备的实体
+                // 忽略免疫“去固体效应”的实体
+                if (entity.getType().is(FLTTags.EntityTypes.DESOLID_EFFECT_IMMUNE)){
+                    return false;
+                }
+                // 对生物实体的判断
+                if (entity instanceof LivingEntity living) {
+                    return !(living.getItemBySlot(EquipmentSlot.BODY).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE) // 忽略穿着减免“去固体效应”的装备的实体
                             || living.getItemBySlot(EquipmentSlot.HEAD).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE) // Todo 这一段以后做探境装备的时候再细化
                             || living.getItemBySlot(EquipmentSlot.CHEST).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE)
                             || living.getItemBySlot(EquipmentSlot.LEGS).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE)
-                            || living.getItemBySlot(EquipmentSlot.FEET).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE));
+                            || living.getItemBySlot(EquipmentSlot.FEET).is(FLTTags.Items.DESOLID_EFFECT_IMMUNE)
+                            || living.hasEffect(MobEffects.LUCK)); // 忽略带有特定药水效果的实体，此处先设为幸运效果
                 }
+                return true;
             }
         }
         return false;
--- src/main/java/com/smallmanseries/farlandstraveler/common/misc/FLTTags.java
+++ src/main/java/com/smallmanseries/farlandstraveler/common/misc/FLTTags.java
@@ -4,7 +4,6 @@ import com.smallmanseries.farlandstraveler.FarLandsTraveler;
 import net.minecraft.core.registries.Registries;
 import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.TagKey;
-import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.item.Item;
 import net.minecraft.world.level.biome.Biome;
