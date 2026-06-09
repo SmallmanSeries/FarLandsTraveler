4777f535492d091f780fb444ed9544d0b5922bd3
INF32768 <3478658394@qq.com>
2026-06-09 22:32:07 +0800

    feat: make Stripe Lands' rendering effects compatible with Sodium Mod

---
+++ src/main/java/com/smallmanseries/farlandstraveler/mixin/sodium/BlockRendererMixin.java
@@ -0,0 +1,21 @@
+package com.smallmanseries.farlandstraveler.mixin.sodium;
+
+import com.smallmanseries.farlandstraveler.Config;
+import net.caffeinemc.mods.sodium.client.render.chunk.compile.pipeline.BlockRenderer;
+import net.minecraft.client.renderer.block.model.BlockStateModel;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.level.block.state.BlockState;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+@Mixin(BlockRenderer.class)
+public abstract class BlockRendererMixin {
+    @Inject(method = "renderModel", at = @At("HEAD"), cancellable = true)
+    private void modifyRenderer(BlockStateModel model, BlockState state, BlockPos pos, BlockPos origin, CallbackInfo ci) {
+        int stripeLandsDistance = Config.STRIPE_LANDS_DISTANCE.getAsInt();
+        if (stripeLandsDistance == -1) return;
+        if (Math.abs(pos.getX()) >= stripeLandsDistance && (pos.getX() & 1) == 1 || Math.abs(pos.getZ()) >= stripeLandsDistance && (pos.getZ() & 1) == 1) ci.cancel();
+    }
+}
