5a7e7d34736532758aa0bc24548263d113e9fb56
OslorasKi <963499487@qq.com>
2026-07-21 23:06:04 +0800

    feat: added cherry cascade feature

--- src/main/java/com/smallmanseries/farlandstraveler/common/worldgen/features/CascadeFeature.java
+++ src/main/java/com/smallmanseries/farlandstraveler/common/worldgen/features/CascadeFeature.java
@@ -1,11 +1,14 @@
 package com.smallmanseries.farlandstraveler.common.worldgen.features;
 
 import com.mojang.serialization.Codec;
+import com.smallmanseries.farlandstraveler.Config;
 import com.smallmanseries.farlandstraveler.common.worldgen.features.configurations.CascadeConfiguration;
 import net.minecraft.core.BlockPos;
+import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.Blocks;
+import net.minecraft.world.level.block.state.BlockBehaviour;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
 import net.minecraft.world.level.levelgen.feature.configurations.TreeConfiguration;
@@ -15,6 +18,10 @@ public class CascadeFeature extends Feature<CascadeConfiguration> {
         super(codec);
     }
 
+    private double baseProb(float distance, float radius) {
+        return 1.0f / (1.0f + Math.exp((distance - radius) / 3.0f));
+    }
+
     @Override
     public boolean place(FeaturePlaceContext<CascadeConfiguration> context) {
         BlockPos origin = context.origin();
@@ -22,8 +29,56 @@ public class CascadeFeature extends Feature<CascadeConfiguration> {
         WorldGenLevel level = context.level();
         CascadeConfiguration config = context.config();
 
-        this.setBlock(level, origin, Blocks.GLOWSTONE.defaultBlockState());
+        float radius = config.radius().sample(random);
+        float yFactor = config.yFactor().sample(random);
+        int size = ((int) radius) + 1;
 
-        return false;
+        float height = 1 / yFactor * radius;
+        float radiusPow = radius * radius;
+        float distancePow;
+        double density;
+
+        // 检查生成高度，如果高于254就生成失败
+        if (origin.getY() > 254) {
+            return false;
+        }
+
+        // 偏移原点至边境之地边缘
+        int farLandsDist = Config.FAR_LANDS_DISTANCE.get();
+        if (origin.getX() > farLandsDist - 3) {
+            origin = origin.offset(-(origin.getX() - farLandsDist + 3), 0, 0);
+        }
+        if (origin.getX() < -farLandsDist) {
+            origin = origin.offset(-(origin.getX() + farLandsDist), 0, 0);
+        }
+        if (origin.getZ() > farLandsDist - 3) {
+            origin = origin.offset(0, 0, -(origin.getZ() - farLandsDist + 3));
+        }
+        if (origin.getZ() < -farLandsDist) {
+            origin = origin.offset(0, 0, -(origin.getZ() + farLandsDist));
+        }
+
+        // 放置
+        for (int i = -size; i <= size; i++) {
+            for (int j = -size; j <= size; j++) {
+                distancePow = i * i + j * j;
+                for (int k = size; k >= -height; k--) {
+                    if (k >= 0) {
+                        density = baseProb((distancePow + k * k), radiusPow) - random.nextDouble() * 0.6;
+
+                    } else {
+                        density = baseProb((distancePow + k * k * yFactor * yFactor), radiusPow) - random.nextDouble() * 0.6;
+                    }
+
+
+                    if (density > 0.5 || (i == 0 && j == 0 && k >= radius - height)) {
+                        this.safeSetBlock(level, origin.offset(new BlockPos(i, k, j)), config.cascadeBlock(), BlockBehaviour.BlockStateBase::canBeReplaced);
+                    }
+                }
+            }
+        }
+
+        this.setBlock(level, origin, config.rootBlock());
+        return true;
     }
 }
--- src/main/java/com/smallmanseries/farlandstraveler/common/worldgen/features/configurations/CascadeConfiguration.java
+++ src/main/java/com/smallmanseries/farlandstraveler/common/worldgen/features/configurations/CascadeConfiguration.java
@@ -2,6 +2,8 @@ package com.smallmanseries.farlandstraveler.common.worldgen.features.configurati
 
 import com.mojang.serialization.Codec;
 import com.mojang.serialization.codecs.RecordCodecBuilder;
+import net.minecraft.util.valueproviders.FloatProvider;
+import net.minecraft.util.valueproviders.FloatProviders;
 import net.minecraft.util.valueproviders.IntProvider;
 import net.minecraft.util.valueproviders.IntProviders;
 import net.minecraft.world.level.block.state.BlockState;
@@ -10,19 +12,17 @@ import net.minecraft.world.level.levelgen.feature.configurations.BlockBlobConfig
 import net.minecraft.world.level.levelgen.feature.configurations.FeatureConfiguration;
 
 public record CascadeConfiguration(
-      //  BlockState rootBlock,
-        BlockState cascadeBlock
-       // IntProvider height,
-       // IntProvider radius,
-        //BlockPredicate canPlaceOn
+        BlockState rootBlock,
+        BlockState cascadeBlock,
+        FloatProvider radius,
+        FloatProvider yFactor
 ) implements FeatureConfiguration {
     public static final Codec<CascadeConfiguration> CODEC = RecordCodecBuilder.create(
             inst -> inst.group(
-                    //BlockState.CODEC.fieldOf("root_block").forGetter(CascadeConfiguration::rootBlock),
-                    BlockState.CODEC.fieldOf("cascade_block").forGetter(CascadeConfiguration::cascadeBlock)
-                    //IntProviders.codec(1, 128).fieldOf("height").forGetter(CascadeConfiguration::height),
-                    //IntProviders.codec(1, 128).fieldOf("radius").forGetter(CascadeConfiguration::radius),
-                    //BlockPredicate.CODEC.fieldOf("can_place_on").forGetter(CascadeConfiguration::canPlaceOn)
+                    BlockState.CODEC.fieldOf("root_block").forGetter(CascadeConfiguration::rootBlock),
+                    BlockState.CODEC.fieldOf("cascade_block").forGetter(CascadeConfiguration::cascadeBlock),
+                    FloatProviders.codec(1, 128).fieldOf("radius").forGetter(CascadeConfiguration::radius),
+                    FloatProviders.codec(0,128).fieldOf("y_factor").forGetter(CascadeConfiguration::yFactor)
             ).apply(inst, CascadeConfiguration::new)
     );
 }
