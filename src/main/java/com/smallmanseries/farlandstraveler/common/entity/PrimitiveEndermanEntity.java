ff7d5554efa9f960bf8b7f06ffca1e9e54a5ece4
Osloras Ki <75368186@qq.com>
2026-08-01 15:52:58 +0800

    feat: primitive enderman escapes water

--- src/main/java/com/smallmanseries/farlandstraveler/common/entity/PrimitiveEndermanEntity.java
+++ src/main/java/com/smallmanseries/farlandstraveler/common/entity/PrimitiveEndermanEntity.java
@@ -1,9 +1,14 @@
 package com.smallmanseries.farlandstraveler.common.entity;
 
+import net.minecraft.core.BlockPos;
 import net.minecraft.core.particles.ParticleTypes;
+import net.minecraft.util.RandomSource;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.ai.goal.Goal;
 import net.minecraft.world.entity.monster.EnderMan;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.phys.Vec3;
 
 public class PrimitiveEndermanEntity extends EnderMan {
     public PrimitiveEndermanEntity(EntityType<? extends EnderMan> type, Level level) {
@@ -12,7 +17,7 @@ public class PrimitiveEndermanEntity extends EnderMan {
 
     protected void registerGoals() {
         super.registerGoals();
-        this.goalSelector.removeGoal(EnderMan.EndermanLookForPlayerGoal);
+        this.goalSelector.addGoal(3, new EscapeWaterGoal(this));
     }
 
     @Override
@@ -34,5 +39,76 @@ public class PrimitiveEndermanEntity extends EnderMan {
     @Override
     protected boolean teleport() {
         return false;
+    }
+
+    @Override
+    protected boolean teleportTowards(Entity entity){
+        return false;
+    }
+
+    private static class EscapeWaterGoal extends Goal {
+        private final PrimitiveEndermanEntity priman;
+        private final Level level;
+        private Vec3 safePlace = null;
+        private boolean escaping;
+
+        public EscapeWaterGoal(PrimitiveEndermanEntity priman){
+            this.priman = priman;
+            this.level = priman.level();
+            this.escaping = false;
+        }
+
+        @Override
+        public boolean canUse() {
+            if(!this.priman.isInWater()){
+                return false;
+            }
+            if (this.priman.getTarget() != null) {
+                return false;
+            }
+            return findSafePlace();
+        }
+
+        private boolean findSafePlace(){
+            if(this.safePlace != null){
+                return true;
+            }
+
+            RandomSource random = this.priman.getRandom();
+            BlockPos pos = this.priman.blockPosition();
+            BlockPos randomPos;
+
+            for(int i = 0; i < 10; i++) {
+                randomPos = pos.offset(random.nextInt(64) - 32, 0, random.nextInt(64) - 32);
+                if (this.level.getBlockState(randomPos).getFluidState().isEmpty()) {
+                    this.safePlace = Vec3.atBottomCenterOf(randomPos);
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        @Override
+        public boolean canContinueToUse() {
+            return !this.priman.getNavigation().isDone();
+        }
+
+        @Override
+        public void start() {
+            this.escaping = true;
+            System.out.println("ks");
+            this.priman.getNavigation().moveTo(this.safePlace.x(), this.safePlace.y(), this.safePlace.z(), 1.0);
+        }
+
+        @Override
+        public void tick(){
+            if(this.escaping && this.priman.onGround() && !this.priman.isInWater()){
+                if(!this.priman.getNavigation().isDone()){
+                    this.priman.getNavigation().stop();
+                }
+                System.out.println("tz");
+                this.escaping = false;
+            }
+        }
     }
 }
